c6651dd987d0b04ac8ba624cb09125e6c2d4b4d9
woojin.kang <woojin.kang@linecorp.com>
2020-03-08 23:14:31 +0900

    init elastic search study

---
+++ elasticsearch/src/test/kotlin/kangwoojin/github/io/elasticsearch/ElasticsearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package kangwoojin.github.io.elasticsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElasticsearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
