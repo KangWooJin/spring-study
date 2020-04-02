06e981f8a476b72e8b3502a4f90a54a1fde5086f
woojin.kang <woojin.kang@linecorp.com>
2020-04-02 20:38:21 +0900

    add spring boot aspect init

---
+++ aspect/src/main/kotlin/kangwoojin/github/aspect/AspectApplication.kt
@@ -0,0 +1,11 @@
+package kangwoojin.github.aspect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AspectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AspectApplication>(*args)
+}
