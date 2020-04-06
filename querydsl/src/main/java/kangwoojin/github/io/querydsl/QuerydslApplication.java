c37654daadfdc24ebbf3aa721f68d7281b648e47
woojin.kang <woojin.kang@linecorp.com>
2020-04-06 22:31:20 +0900

    Add query dsl project init

---
+++ querydsl/src/main/java/kangwoojin/github/io/querydsl/QuerydslApplication.java
@@ -0,0 +1,13 @@
+package kangwoojin.github.io.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslApplication.class, args);
+    }
+
+}
