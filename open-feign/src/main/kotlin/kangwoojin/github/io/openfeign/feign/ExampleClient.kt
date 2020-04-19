e563fb37a34d9c435352da956f0fdabcd9d8ddb4
woojin.kang <woojin.kang@linecorp.com>
2020-04-19 11:46:39 +0900

    open-feign init

--- open-feign/src/main/kotlin/kangwoojin/github/io/openfeign/feign/ExampleClient.kt
+++ open-feign/src/main/kotlin/kangwoojin/github/io/openfeign/feign/ExampleClient.kt
@@ -1,11 +1,12 @@
 package kangwoojin.github.io.openfeign.feign
 
+import org.apache.juli.logging.LogConfigurationException
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.cloud.openfeign.SpringQueryMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "example", url = "http://localhost:8080")
+@FeignClient(name = "example", url = "http://localhost:8080", configuration = [LogConfigurationException::class])
 interface ExampleClient {
 
     @GetMapping("/success")
--- open-feign/src/main/kotlin/kangwoojin/github/io/openfeign/feign/LoggingConfiguration.kt
+++ open-feign/src/main/kotlin/kangwoojin/github/io/openfeign/feign/LoggingConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class FooConfiguration {
+class LoggingConfiguration {
     @Bean
     fun feignLoggerLevel(): Logger.Level {
         return Logger.Level.FULL
