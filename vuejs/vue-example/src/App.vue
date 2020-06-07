8cad8a4220ca224a556a6c86258eebefe4f91f31
woojin.kang <woojin.kang@linecorp.com>
2020-06-07 15:46:22 +0900

    Fix lint

--- vuejs/vue-example/src/App.vue
+++ vuejs/vue-example/src/App.vue
@@ -10,24 +10,24 @@
 </template>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2C3E50;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  #nav {
-    padding: 30px;
+#nav {
+  padding: 30px;
 
-    a {
-      font-weight: bold;
-      color: #2C3E50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-      &.router-link-exact-active {
-        color: #42B983;
-      }
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
+}
 </style>
