1bcaa32bce21eb7d39992c8dbb7d0e5f3a61ce97
woojin.kang <woojin.kang@linecorp.com>
2020-06-07 13:06:42 +0900

    Add vuejs init

---
+++ vuejs/src/main/kotlin/kangwoojin/github/io/vuejs/VuejsApplication.kt
@@ -0,0 +1,11 @@
+package kangwoojin.github.io.vuejs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VuejsApplication
+
+fun main(args: Array<String>) {
+    runApplication<VuejsApplication>(*args)
+}
---
+++ vuejs/src/test/kotlin/kangwoojin/github/io/vuejs/VuejsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kangwoojin.github.io.vuejs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VuejsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
