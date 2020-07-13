614dde48510442e593e9c8bb39d2950562e4372a
woojin.kang <woojin.kang@linecorp.com>
2020-07-13 19:16:56 +0900

    Add gradle demo

---
+++ gradle-demo/src/test/java/kangwoojin/github/io/gradledemo/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kangwoojin.github.io.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
