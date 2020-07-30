fdb21f5b50398c42c8dc2f8599b40dd1db772920
woojin.kang <woojin.kang@linecorp.com>
2020-07-30 22:26:11 +0900

    Add Spring Security example

---
+++ spring-security/src/main/java/kangwoojin/github/io/springsecurity/SimpleController.java
@@ -0,0 +1,39 @@
+package kangwoojin.github.io.springsecurity;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("message", "Hello Spring" + principal.getName());
+        } else {
+            model.addAttribute("message", "Hello Spring Security");
+        }
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Hello Info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "Hello" + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello admin" + principal.getName());
+        return "admin";
+    }
+}
---
+++ spring-security/src/main/java/kangwoojin/github/io/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kangwoojin.github.io.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .mvcMatchers("/", "/info").permitAll()
+            .mvcMatchers("admin").hasAnyRole("ADMIN")
+            .anyRequest().authenticated();
+
+        http.formLogin();
+        http.httpBasic();
+    }
+}
